adc9807cc392bbd188f4ecf5f69e472ee0f5bfcd
khashayarRohani <khashayarrhi@gmail.com>
2024-07-06 01:05:10 +0100

    Web is Ready

--- app.js
+++ app.js
@@ -5,3 +5,15 @@ document.addEventListener("mousemove", (e) => {
   let y = e.clientY - document.documentElement.clientHeight * 1.5;
   shadow.style.transform = "translate(" + x + "px, " + y + "px)";
 });
+
+function openLoginWindow() {
+  const width = 1080;
+  const height = 720;
+  const left = (screen.width - width) / 2;
+  const top = (screen.height - height) / 2;
+  window.open(
+    "login.html", // URL of the login page
+    "Login", // Window name
+    `width=${width},height=${height},top=${top},left=${left}`
+  );
+}
